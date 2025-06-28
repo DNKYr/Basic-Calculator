64047793846481bc49c099aa53eb35980547b699
David Li <dnkyr2007@gmail.com>
2025-06-28 17:22:12 -0400

    Adding __name__ feature to increase code's scalability and reusabability

--- Calculator_logic.py
+++ Calculator_logic.py
@@ -20,4 +20,25 @@ def print_welcome_message():
 
 def print_outro_message():
     print("Thank you for using basic calculator. ")
-if __name__ == "__main__":    repeat = True    print_welcome_message()    while repeat:        try:             first_number = int(input("Please input the first number \n"))            operation_type = str(input("Now please input the operation you want to perform (eg. +, -, *, /)\n"))            second_number = int(input("Please input the second number\n"))            result = calculate(first_number, second_number, operation_type)            print(result)        except ZeroDivisionError as e:            print(f"ZeroDvisionError: Division by Zero is undefined")        except ValueError as e:            print(f"ValueError: {e}")                r = str(input("Do you want to end this program? If yes please enter: STOP \n"))        if r.upper() == "STOP":            repeat = False    print_outro_message()
+
+if __name__ == "__main__":
+    repeat = True
+    print_welcome_message()
+    while repeat:
+        try: 
+            first_number = int(input("Please input the first number \n"))
+            operation_type = str(input("Now please input the operation you want to perform (eg. +, -, *, /)\n"))
+            second_number = int(input("Please input the second number\n"))
+
+            result = calculate(first_number, second_number, operation_type)
+            print(result)
+        except ZeroDivisionError as e:
+            print(f"ZeroDvisionError: Division by Zero is undefined")
+        except ValueError as e:
+            print(f"ValueError: {e}")
+    
+        r = str(input("Do you want to end this program? If yes please enter: STOP \n"))
+        if r == "STOP":
+            repeat = False
+
+    print_outro_message()
